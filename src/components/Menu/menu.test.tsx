da2de8a4d746e1b39500eafe97cbe5c518b9073a
evankwolf <452367115@qq.com>
2023-05-15 10:52:48 +0800

    feat: init menu test cases

---
+++ src/components/Menu/menu.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { render } from '@testing-library/react'
+import { describe, it } from 'vitest'
+
+describe('test Menu and MenuItem component.', () => {
+  it('should render correct Menu and MenuItem based on default props', () => {
+
+  })
+  it('should change active and call the right callback when a menu item is clicked', () => {
+
+  })
+  it('should render vertical mode when mode is set to vertical', () => {
+
+  })
+})
