cfe5e69fec6ec1baffb2cdedcec283a8b7643f11
evankwolf <452367115@qq.com>
2023-05-18 11:40:58 +0800

    docs: add some annotations for AlertProps

--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -9,9 +9,13 @@ type AlertType = 'success' | 'default' | 'danger' | 'warning'
 
 export interface AlertProps {
   children?: string | React.ReactNode
+  /** Alert type, different types represent different colors */
   type?: AlertType
+  /** Alert header text */
   header?: string
+  /** Set true to show close button and make the component closable */
   closable?: boolean
+  /** callback when alert is closed through the close button */
   onClose?: () => void
 }
 
