0269747eb5cc726589724b42d5686eea968630ce
evankwolf <452367115@qq.com>
2023-05-28 02:01:50 +0800

    Updates

--- components/AutoComplete/autoComplete.d.ts
+++ components/AutoComplete/autoComplete.d.ts
@@ -5,6 +5,11 @@ interface DataSourceObject {
 }
 export type DataSourceType<T = {}> = T & DataSourceObject;
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
+    /**
+     * @function fetchSuggestions
+     *
+     * @param {string} keyword input value
+     */
     fetchSuggestions: (keyword: string) => DataSourceType[] | Promise<DataSourceType[]>;
     onSelect: (suggestion: DataSourceType) => void;
     renderOption?: (data: DataSourceType) => React.ReactNode;
--- components/Icon/icon.stories.d.ts
+++ components/Icon/icon.stories.d.ts
@@ -3,9 +3,11 @@ import type { Meta, StoryFn } from '@storybook/react';
 type Story = StoryFn<typeof Icon>;
 declare const IconMeta: Meta<typeof Icon>;
 export default IconMeta;
-export declare const Playground: Story;
 /**
- * some annotations
- * @returns
+ * All of the icons are based on [FontAwesome](https://fontawesome.com/). The awesome icon library and toolkit.
+ *
+ * You can simply search for the icon you like through [here](https://fontawesome.com/search).
+ *
+ * You can use `set string` in the control panel below to test. For example, try to set `icon` as `arrow-up` and see what is going to happen :)
  */
 export declare const ArrowDown: Story;
